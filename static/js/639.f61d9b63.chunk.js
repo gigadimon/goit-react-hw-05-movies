c7dadf30f9222a7dbd648ff466e0fae460c044e3
gigadimon <94648562+gigadimon@users.noreply.github.com>
2022-08-08 19:58:56 +0000

    Deploying to gh-pages from @ gigadimon/goit-react-hw-05-movies@a22ae083c268cdb19c29f5c45a7892629a6719f4 🚀

---
+++ static/js/639.f61d9b63.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[639],{2639:function(e,r,a){a.r(r),a.d(r,{default:function(){return c}});var l=a(3924),t="Loader_container__X02md",o="Loader_loader__+lRPl",i=a(184);function c(){return(0,i.jsx)("div",{className:t,children:(0,i.jsx)(l.Z1,{className:o,height:"100",width:"100",color:"#4fa94d",wrapperStyle:{},wrapperClass:"",visible:!0,ariaLabel:"three-circles-rotating",outerCircleColor:"",innerCircleColor:"",middleCircleColor:""})})}}}]);
+//# sourceMappingURL=639.f61d9b63.chunk.js.map
